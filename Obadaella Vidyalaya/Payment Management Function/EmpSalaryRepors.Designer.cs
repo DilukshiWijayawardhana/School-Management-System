946e96590218a2ac4f5985ab884b9c75240a4526
DilukshiWijayawardhana <dilukshi1017@gmail.com>
2022-06-28 14:04:02 +0530

    Add project files.

---
+++ Obadaella Vidyalaya/Payment Management Function/EmpSalaryRepors.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Obadaella_Vidyalaya
+{
+    partial class EmpSalaryRepors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmpSalaryRepors));
+            this.dgvEmpSalReports = new System.Windows.Forms.DataGridView();
+            this.txtSearchEmpSalReport = new System.Windows.Forms.TextBox();
+            this.btnEmpSalReport = new System.Windows.Forms.Button();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox19 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpSalReports)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmpSalReports
+            // 
+            this.dgvEmpSalReports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpSalReports.Location = new System.Drawing.Point(1431, 0);
+            this.dgvEmpSalReports.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvEmpSalReports.Name = "dgvEmpSalReports";
+            this.dgvEmpSalReports.RowHeadersWidth = 51;
+            this.dgvEmpSalReports.Size = new System.Drawing.Size(13, 12);
+            this.dgvEmpSalReports.TabIndex = 0;
+            this.dgvEmpSalReports.Visible = false;
+            // 
+            // txtSearchEmpSalReport
+            // 
+            this.txtSearchEmpSalReport.Location = new System.Drawing.Point(1452, 0);
+            this.txtSearchEmpSalReport.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearchEmpSalReport.Name = "txtSearchEmpSalReport";
+            this.txtSearchEmpSalReport.Size = new System.Drawing.Size(12, 22);
+            this.txtSearchEmpSalReport.TabIndex = 1;
+            this.txtSearchEmpSalReport.Visible = false;
+            this.txtSearchEmpSalReport.TextChanged += new System.EventHandler(this.txtSearchEmpSalReport_TextChanged);
+            // 
+            // btnEmpSalReport
+            // 
+            this.btnEmpSalReport.Location = new System.Drawing.Point(1465, 0);
+            this.btnEmpSalReport.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEmpSalReport.Name = "btnEmpSalReport";
+            this.btnEmpSalReport.Size = new System.Drawing.Size(13, 12);
+            this.btnEmpSalReport.TabIndex = 2;
+            this.btnEmpSalReport.Text = "button1";
+            this.btnEmpSalReport.UseVisualStyleBackColor = true;
+            this.btnEmpSalReport.Visible = false;
+            this.btnEmpSalReport.Click += new System.EventHandler(this.btnEmpSalReport_Click);
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 138);
+            this.crystalReportViewer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1478, 648);
+            this.crystalReportViewer1.TabIndex = 3;
+            this.crystalReportViewer1.ToolPanelWidth = 267;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel3.BackColor = System.Drawing.Color.Maroon;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.pictureBox19);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.MaximumSize = new System.Drawing.Size(1487, 180);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1487, 138);
+            this.panel3.TabIndex = 68;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 23);
+            this.pictureBox1.TabIndex = 40;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(445, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(643, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ඕබඩඇල්ල විද්‍යාලය,බණ්ඩාරවෙල";
+            // 
+            // pictureBox19
+            // 
+            this.pictureBox19.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox19.Image")));
+            this.pictureBox19.Location = new System.Drawing.Point(681, 2);
+            this.pictureBox19.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox19.MaximumSize = new System.Drawing.Size(121, 110);
+            this.pictureBox19.Name = "pictureBox19";
+            this.pictureBox19.Size = new System.Drawing.Size(103, 78);
+            this.pictureBox19.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox19.TabIndex = 0;
+            this.pictureBox19.TabStop = false;
+            // 
+            // EmpSalaryRepors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1481, 786);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.btnEmpSalReport);
+            this.Controls.Add(this.txtSearchEmpSalReport);
+            this.Controls.Add(this.dgvEmpSalReports);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EmpSalaryRepors";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EmpSalaryRepors";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpSalReports)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmpSalReports;
+        private System.Windows.Forms.TextBox txtSearchEmpSalReport;
+        private System.Windows.Forms.Button btnEmpSalReport;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox19;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
